a7f5d62a2914ea0d0be23252dd7a1c5b73610321
Renan Constancio <renan@dcisuporte.com.br>
2022-06-20 16:16:06 -0300

    Ajuste simples

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,6 +5,6 @@ import { dataSource } from '../typeorm';
 
 dataSource.initialize().then(() => {
   app.listen(process.env.PORT || 3333, () => {
-    console.log(`Server started on port ${process.env.PORT || 3333}! 🏆`);
+    console.log(`Server started on port ${process.env.PORT || 3333}`);
   });
 });
