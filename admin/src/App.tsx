9d8fe9d06afb0c65960e2c6fbce368c8a7cb8d08
Renan Constancio <renan@dcisuporte.com.br>
2022-07-02 10:06:34 -0300

    .

---
+++ admin/src/App.tsx
@@ -0,0 +1,14 @@
+import Login from "./pages/Login";
+import "./style/app.scss";
+
+function App() {
+  return <Login />;
+  // return (
+  //   <div className="app">
+  //     <nav></nav>
+  //     <aside>s</aside>
+  //   </div>
+  // );
+}
+
+export default App;
